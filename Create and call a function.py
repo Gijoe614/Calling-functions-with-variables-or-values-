147d5da3a7c460fcf539151d7815df4af2e3f93d
Gijoe614 <jvandoorn@students.mchenry.edu>
2016-09-08 14:02:10 -0500

    Delete Create and call a function.py

--- Create and call a function.py
+++
@@ -1,9 +0,0 @@
-def sing_childsprayer():
-    print("Hush my dear!")
-    print("Lie still and slumber!")
-    print("Holy angels guard thy bed,")
-    print("Heavenly blessings, without number,")
-    print("Gently falling on thy head.")
-
-sing_childsprayer()
-sing_childsprayer()
