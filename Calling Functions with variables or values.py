18a4099dea6356ee3c11bf1da127c82b7ddf8950
Gijoe614 <jvandoorn@students.mchenry.edu>
2016-09-08 14:02:42 -0500

    Add files via upload

---
+++ Calling Functions with variables or values.py
@@ -0,0 +1,18 @@
+
+
+def pay(hours, pay):
+
+    my_pay = pay-hours
+    str_pay = str(my_pay)
+    print("I earn " + str_pay + " pay_rate")
+
+    hours_worked = 40
+    pay_rate = 10
+
+    pay(hours_worked, pay_rate)
+
+    pay(40, 10)
+
+
+
+
